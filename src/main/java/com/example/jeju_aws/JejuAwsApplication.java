e5258ab93b83d0e167c7dc210d492af4248806fc
docherryra <docherryra@gmail.com>
2025-01-20 20:22:06 +0900

    initial setting

---
+++ src/main/java/com/example/jeju_aws/JejuAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.jeju_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejuAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JejuAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jeju_aws/JejuAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jeju_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejuAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
